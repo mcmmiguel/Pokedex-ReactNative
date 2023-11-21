eaf349bfd0a796c1589724e8da5e8d7698b90405
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-21 11:31:26 -0600

    Chore. Set up homescreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { appTheme } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
+
+    const { top } = useSafeAreaInsets();
+
     return (
         <View>
-            <Text>HomeScreen</Text>
+            <Image
+                source={require('../assets/pokebola.png')}
+                style={appTheme.pokebolaBG}
+            />
+            <Text style={{
+                ...appTheme.title,
+                ...appTheme.globalMargin,
+                marginTop: top + 20,
+            }}>Pokedex</Text>
         </View>
     );
 };
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const appTheme = StyleSheet.create({
+    globalMargin: {
+        marginHorizontal: 20,
+    },
+    pokebolaBG: {
+        position: 'absolute',
+        top: -100,
+        right: -100,
+        opacity: 0.2,
+        width: 300,
+        height: 300,
+    },
+    title: {
+        fontSize: 35,
+        fontWeight: 'bold',
+    },
+});
