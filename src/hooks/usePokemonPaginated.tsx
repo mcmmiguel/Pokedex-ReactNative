3cd63ca8728df077393b1169c337ed9b97617269
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-21 12:25:56 -0600

    Feat. Get pokemon list

---
+++ src/hooks/usePokemonPaginated.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
+import { pokemonAPI } from '../api';
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces';
+
+export const usePokemonPaginated = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
+
+    const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
+
+    const loadPokemons = async () => {
+
+        setIsLoading(true);
+        const resp = await pokemonAPI.get<PokemonPaginatedResponse>(nextPageUrl.current);
+        nextPageUrl.current = resp.data.next;
+        mapPokemonList(resp.data.results);
+
+    };
+
+    const mapPokemonList = (pokemonList: Result[]) => {
+
+        const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+
+            const urlPaths = url.split('/');
+            const id = urlPaths[urlPaths.length - 2];
+            const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+
+            return { id, picture, name };
+        });
+
+        setSimplePokemonList({ ...setSimplePokemonList, ...newPokemonList });
+        setIsLoading(false);
+
+    };
+
+    useEffect(() => {
+        loadPokemons();
+    }, []);
+
+    return {
+        isLoading,
+        simplePokemonList,
+    };
+
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { appTheme } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { usePokemonPaginated } from '../hooks';
 
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
+    const { isLoading, simplePokemonList } = usePokemonPaginated();
+    console.log(simplePokemonList);
 
     return (
         <View>
