6e4b0ae7ca97ee5bd2316278de8bbd1d5a4a1816
Miguel Cobian <miguelcobian99@gmail.com>
2023-11-21 18:07:45 -0600

    Feat. Weight, sprites and moves

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import { PokemonFull } from '../interfaces';
+import { FadeInImage } from './FadeInImage';
 
 export interface PokemonDetailsProps {
     pokemon: PokemonFull;
@@ -9,23 +10,53 @@ export interface PokemonDetailsProps {
 export const PokemonDetails = ({ pokemon }: PokemonDetailsProps) => {
     return (
         <ScrollView
+            showsVerticalScrollIndicator={false}
             style={styles.mainContainer}
         >
-            {/* Types */}
+            {/* Types y peso */}
             <View style={styles.typesContainer}>
                 <Text style={styles.title}>Types</Text>
                 <View style={styles.row}>
-
                     {pokemon.types.map(({ type }) => (
                         <Text key={type.name} style={styles.regularText}>{type.name}</Text>
                     ))}
                 </View>
+                <Text style={styles.title}>Weight</Text>
+                <Text style={styles.regularText}>{pokemon.weight} lb</Text>
             </View>
 
             {/* Sprites */}
-            <View style={styles.spritesContainer}>
+            <View style={styles.regularContainer}>
                 <Text style={styles.title}>Sprites</Text>
             </View>
+
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+                <FadeInImage style={styles.basicSprite} uri={pokemon.sprites.front_default} />
+                <FadeInImage style={styles.basicSprite} uri={pokemon.sprites.back_default} />
+                <FadeInImage style={styles.basicSprite} uri={pokemon.sprites.front_shiny} />
+                <FadeInImage style={styles.basicSprite} uri={pokemon.sprites.back_shiny} />
+            </ScrollView>
+
+            {/* Habilidades */}
+            <View style={styles.regularContainer}>
+                <Text style={styles.title}>Base Skills</Text>
+                <View style={styles.row}>
+                    {pokemon.abilities.map(({ ability }) => (
+                        <Text key={ability.name} style={styles.regularText}>{ability.name}</Text>
+                    ))}
+                </View>
+            </View>
+
+            {/* Movimientos */}
+            <View style={styles.regularContainer}>
+                <Text style={styles.title}>Moves</Text>
+                <View style={styles.row}>
+                    {pokemon.moves.map(({ move }) => (
+                        <Text key={move.name} style={styles.regularText}>{move.name}</Text>
+                    ))}
+                </View>
+            </View>
+
         </ScrollView>
     );
 };
@@ -40,17 +71,22 @@ const styles = StyleSheet.create({
     },
     row: {
         flexDirection: 'row',
+        flexWrap: 'wrap',
     },
     title: {
         fontSize: 20,
+        marginTop: 20,
         fontWeight: 'bold',
     },
     regularText: {
         fontSize: 17,
         marginRight: 10,
     },
-    spritesContainer: {
-        marginTop: 20,
+    regularContainer: {
         marginHorizontal: 20,
+    },
+    basicSprite: {
+        width: 100,
+        height: 100,
     },
 });
